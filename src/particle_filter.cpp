b0b16bd89ff311e3df1690a7007da52d03433a4f
JieqingShi <jieqingshi@googlemail.com>
2019-11-21 16:37:17 +0000

    Trying new resample() function

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -286,13 +286,9 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   }
 }
 
+/*
 void ParticleFilter::resample() {
-  /**
-   * TODO: Resample particles with replacement with probability proportional 
-   *   to their weight. 
-   * NOTE: You may find std::discrete_distribution helpful here.
-   *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
-   */
+
   vector<Particle> sampledParticles(num_particles);
   //std::default_random_engine gen;
   
@@ -321,6 +317,45 @@ void ParticleFilter::resample() {
   std::cout<<"Sampled "<<sampledParticles.size()<<"particles \n";
   particles = sampledParticles;
 }
+*/
+void ParticleFilter::resample() {
+  // TODO: Resample particles with replacement with probability proportional to their weight. 
+  // NOTE: You may find std::discrete_distribution helpful here.
+  //   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  
+  vector<Particle> new_particles;
+
+  // get all of the current weights
+  vector<double> weights;
+  for (int i = 0; i < num_particles; i++) {
+    weights.push_back(particles[i].weight);
+  }
+
+  // generate random starting index for resampling wheel
+  std::uniform_int_distribution<int> uniintdist(0, num_particles-1);
+  auto index = uniintdist(gen);
+
+  // get max weight
+  double max_weight = *max_element(weights.begin(), weights.end());
+
+  // uniform random distribution [0.0, max_weight)
+  std::uniform_real_distribution<double> unirealdist(0.0, max_weight);
+
+  double beta = 0.0;
+
+  // spin the resample wheel!
+  for (int i = 0; i < num_particles; i++) {
+    beta += unirealdist(gen) * 2.0;
+    while (beta > weights[index]) {
+      beta -= weights[index];
+      index = (index + 1) % num_particles;
+    }
+    new_particles.push_back(particles[index]);
+  }
+
+  particles = new_particles;
+}
+
 
 void ParticleFilter::SetAssociations(Particle& particle, 
                                      const vector<int>& associations, 
